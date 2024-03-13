d50b5c28f8c19dfaebca8ad142a159f816bc1b76
Taziwa-Mushayabasa <tazmush@live.com>
2024-03-13 19:26:22 +0000

    updated pipeline to be secured with https

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,12 @@ sudo apt update && sudo apt install nodejs npm
 
 sudo npm install -g pm2
 
-pm2 stop simple_app
+pm2 stop example_app
 
-cd SimpleApplication/
+cd ExampleApplication/
 
 npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
 
-pm2 start ./bin/www --name simple_app
+pm2 start ./bin/www --name example_app
